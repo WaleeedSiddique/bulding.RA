17dc5036ae1975b49e65e9d1afdbda90b8caa578
Waleed Siddique <waleed.heaven007@gmail.com>
2023-02-02 15:16:15 +0500

    file updated

---
+++ building-side.js
@@ -0,0 +1,13 @@
+
+function openSidebar(){
+    let a = document.getElementById("mySidebar").classList.toggle("sidebar-show");
+    // let b = document.getElementById("content");
+    // console.log
+    // if (document.getElementById("mySidebar").style.display == "block") {
+    //     console.log(a)
+    // }
+    // else{
+    //     console.log("raheel")
+    // }
+} 
+
